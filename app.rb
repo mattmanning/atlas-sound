c434979aebc007744a86bac396ccf4c0f909c4c3
Matt Manning <matt.manning@gmail.com>
2019-01-23 00:19:46 -0500

    filter issue update by team

--- app.rb
+++ app.rb
@@ -14,8 +14,9 @@ class App < Sinatra::Base
 
     case payload['webhookEvent']
     when 'jira:issue_updated'
-      status = payload ['issue']['fields']['status']['name']
-      changelog_items = payload['changelog']['items'].map {|i| i["toString"]}
+      status = payload['issue']['fields']['status']['name']
+      changelog_items = payload['changelog']['items'].map {|i| i['toString']}
+      team = payload['issue']['fields']['customfield_12012']['value']
     when 'sprint_started'
       sprint_started(payload['sprint']['name'])
     end
@@ -29,8 +30,8 @@ class App < Sinatra::Base
     end
   end
 
-  def issue_updated(status, changelog_items)
-    if status == 'Done' && changelog_items.include?('Done')
+  def issue_updated(status, changelog_items, team)
+    if status == 'Done' && changelog_items.include?('Done') && team == 'Atlas'
       SoundWorker.perform_async(:issue_done)
     end
   end
