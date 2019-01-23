99abc6603859004e0551ec368faaff315d664f0e
Matt Manning <matt.manning@gmail.com>
2019-01-23 16:33:36 -0500

    use the job to play the sounds

--- lib/workers/sound_worker.rb
+++ lib/workers/sound_worker.rb
@@ -1,7 +1,13 @@
 class SoundWorker
   include Sidekiq::Worker
 
+  SOUNDS = {
+    sprint_started: 'who_let_the_dogs_out.mp3',
+    issue_done: 'oh_yeah.mp3'
+  }
+
   def perform(event)
     puts event.inspect
+    `omxplayer #{SOUNDS[event]}`
   end
 end
