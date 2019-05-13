617399e88d3ceb8737fab271dd95c9e747380d37
Matt Manning <matt.manning@gmail.com>
2019-05-13 16:29:57 -0400

    can't nobody tell me nothin

--- lib/workers/sound_worker.rb
+++ lib/workers/sound_worker.rb
@@ -4,7 +4,7 @@ class SoundWorker
   SOUNDS = {
     issue_done: 'all_i_do_is_win.mp3',
     prod_push: 'push_it.mp3',
-    sprint_started: 'rocky.mp3'
+    sprint_started: 'old_town_road.mp3'
   }
 
   def perform(event)
