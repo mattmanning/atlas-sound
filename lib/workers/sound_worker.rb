a6102d83e969d1e155929eaa7f607b416721f403
Matt Manning <matt.manning@gmail.com>
2019-09-03 10:54:08 -0400

    baby shark

--- lib/workers/sound_worker.rb
+++ lib/workers/sound_worker.rb
@@ -4,7 +4,7 @@ class SoundWorker
   SOUNDS = {
     issue_done: 'all_i_do_is_win.mp3',
     prod_push: 'push_it.mp3',
-    sprint_started: 'sicko_mode.mp3'
+    sprint_started: 'baby_shark.mp3'
   }
 
   def perform(event)
